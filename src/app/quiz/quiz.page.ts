7dba9071d22a56fd0aa9427d5d11e5bade734e61
aluno <etecae023@hotmail.com>
2025-06-10 20:29:48 -0300

    tema

--- src/app/quiz/quiz.page.ts
+++ src/app/quiz/quiz.page.ts
@@ -16,51 +16,61 @@ export class QuizPage implements OnInit {
 
   questions = [
     {
+      theme:'League of Legends',
       text: 'Qual campeão é conhecido como "o Amigo da Floresta"?',
       options: ['Amumu', 'Ivern', 'Maokai', 'Zac'],
       answer: 'Ivern'
     },
     {
+      theme:'League of Legends',
       text: 'Qual é o papel principal do campeão Thresh?',
       options: ['Atirador', 'Caçador', 'Suporte', 'Top Lane'],
       answer: 'Suporte'
     },
     {
+      theme:'League of Legends',
       text: 'Qual desses dragões concede velocidade de movimento?',
       options: ['Dragão Infernal', 'Dragão da Montanha', 'Dragão das Nuvens', 'Dragão do Oceano'],
       answer: 'Dragão das Nuvens'
     },
     {
+      theme:'League of Legends',
       text: 'Qual campeão tem uma ultimate chamada "Investida do Veloz"?',
       options: ['Hecarim', 'Rammus', 'Volibear', 'Udyr'],
       answer: 'Hecarim'
     },
     {
+      theme:'League of Legends',
       text: 'Qual foi o primeiro campeão lançado em League of Legends?',
       options: ['Singed', 'Ashe', 'Ryze', 'Annie'],
       answer: 'Singed'
     },
     {
+      theme:'Clash Royale',
       text: 'Qual carta custa 1 de elixir?',
       options: ['Esqueletos', 'Mosqueteira', 'Golem', 'Gigante'],
       answer: 'Esqueletos'
     },
     {
+      theme:'Clash Royale',
       text: 'Qual é a raridade da carta "Mago de Gelo"?',
       options: ['Comum', 'Rara', 'Épica', 'Lendária'],
       answer: 'Lendária'
     },
     {
+      theme:'Clash Royale',
       text: 'Qual estrutura causa dano em área e explode ao ser destruída?',
       options: ['Canhão', 'Torre Inferno', 'Tesla', 'Torre Bomba'],
       answer: 'Torre Bomba'
     },
     {
+      theme:'Clash Royale',
       text: 'Qual dessas cartas voadoras não ataca unidades terrestres e aéreas?',
       options: ['Dragão Infernal', 'Servos', 'Bebê Dragão', 'Balão'],
       answer: 'Balão'
     },
     {
+      theme:'Clash Royale',
       text: 'Quantas cartas você pode ter em seu deck principal?',
       options: ['6', '8', '10', '12'],
       answer: '8'
